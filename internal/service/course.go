47e4e42addec82860b0c0274a30abb63aa8f7290
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-11-14 05:41:15 -0300

    feat: add course creation functionality with streaming support

--- internal/service/course.go
+++ internal/service/course.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"context"
+	"io"
+
 	"github.com/zHenriqueGN/AppPC/internal/database"
+	"github.com/zHenriqueGN/AppPC/internal/entity"
 	"github.com/zHenriqueGN/AppPC/internal/pb"
 )
 
@@ -13,5 +17,69 @@ type CourseService struct {
 func NewCourseService(courseDB database.CourseDB) *CourseService {
 	return &CourseService{
 		CourseDB: courseDB,
+	}
+}
+
+func (c *CourseService) CreateCourse(ctx context.Context, in *pb.CreateCourseRequest) (*pb.CourseResponse, error) {
+	course := entity.NewCourse(in.Name, in.Description, in.CategoryID)
+	err := c.CourseDB.Create(course)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.CourseResponse{
+		Id:          course.ID,
+		Name:        course.Name,
+		Description: course.Description,
+		CategoryID:  course.CategoryID,
+	}, nil
+}
+
+func (c *CourseService) CreateCourseStream(stream pb.CourseService_CreateCourseStreamServer) error {
+	courses := &pb.CourseListResponse{}
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(courses)
+		}
+		if err != nil {
+			return err
+		}
+		course := entity.NewCourse(in.Name, in.Description, in.CategoryID)
+		err = c.CourseDB.Create(course)
+		if err != nil {
+			return err
+		}
+		courses.Courses = append(courses.Courses, &pb.CourseResponse{
+			Id:          course.ID,
+			Name:        course.Name,
+			Description: course.Description,
+			CategoryID:  course.CategoryID,
+		})
+	}
+}
+
+func (c *CourseService) CreateCourseStreamBidirectional(stream pb.CourseService_CreateCourseStreamBidirectionalServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		course := entity.NewCourse(in.Name, in.Description, in.CategoryID)
+		err = c.CourseDB.Create(course)
+		if err != nil {
+			return err
+		}
+		err = stream.Send(&pb.CourseResponse{
+			Id:          course.ID,
+			Name:        course.Name,
+			Description: course.Description,
+			CategoryID:  course.CategoryID,
+		})
+		if err != nil {
+			return err
+		}
 	}
 }
